24b69a885d1af79a7e10383fcecf639f9b8618bb
siman <951400676@qq.com>
2021-02-17 03:09:22 +0800

    feat:add SPI

---
+++ simple-Qrpc-common/src/main/java/com/siman/qrpc/extension/SPI.java
@@ -0,0 +1,14 @@
+package com.siman.qrpc.extension;
+
+import java.lang.annotation.*;
+
+/**
+ * @author SiMan
+ * @date 2021/2/5 15:12
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SPI {
+}
